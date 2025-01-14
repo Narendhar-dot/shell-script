974c3ff19d50c5672c4e196d011efaba163d5edb
narendhar <narendhar.v@gmail.com>
2025-01-14 11:01:55 +0100

    new variables added

---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=RAMESH
+PERSON2=SURESH
+echo "$PERSON1:: Hi $PERSON2"
+echo "$PERSON2:: Hellow $PERSON1"
+echo "$PERSON1:: how are you doing?"
+echo "$PERSON2:: I am great.. How are you? "
