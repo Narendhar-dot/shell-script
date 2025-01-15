922652037e8d525e08e7e064804f335e02359e83
narendhar <narendhar.v@gmail.com>
2025-01-15 10:58:31 +0100

    add

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
 NUMBER1=$1
 NUMBER2=$2
-Timestamp=$(date)
-TIME="Sum of the 2 numbers counted at $Timestamp"
-COUNT=$(($NUMBER1+$NUMBER2))
+
+TIMESTAMP=$(date)
+echo "script executed at : $TIMESTAMP"
+
+
