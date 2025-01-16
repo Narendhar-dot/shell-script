652c056bb54603affa6e0f84195acd3c685e07df
narendhar <narendhar.v@gmail.com>
2025-01-16 12:33:58 +0100

    install-script

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -6,5 +6,29 @@ then
     exit 1
 fi
 dnf list installed mysql
-
+if [$? -ne 0]
+then 
+    dnf install mysql -y
+    if [$? -ne 0]
+    then 
+        echo "Installation of mysql---FAILED"
+    else
+        echo "Installation of mysql---succeded"
+    fi
+else 
+    echo "MYSQL Installed already!!!!"
+fi
+dnf list installed git
+if [$? -ne 0]
+then   
+    dnf install git -y
+    if [$? -ne 0]
+    then 
+        echo "Installation of git...Failure"
+    else
+        echo "Installation og git...success"
+    fi
+else 
+    echo "Git Installed already!!!!"
+fi
 
