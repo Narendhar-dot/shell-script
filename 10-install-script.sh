2b77dfb3f0eee808b384d3d400b3a687825efff5
narendhar <narendhar.v@gmail.com>
2025-01-16 12:19:47 +0100

    install-script

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -6,15 +6,5 @@ then
     exit 1
 fi
 dnf list installed mysql
-if [$? -ne 0]
-then
-    dnf install mysql -y
-    if[$? -ne 0]
-    then
-        echo "Installation of mysql...FAILURE"
-    else
-        echo "Installation of mysql...SUCCESS"
-else
-    echo "MYSQL INSTALLED"
-fi
+
 
