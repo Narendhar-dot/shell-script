f5c6de2a8fff426912a15ed5f9670a8734da1885
narendhar <narendhar.v@gmail.com>
2025-01-16 12:15:07 +0100

    install-script

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -3,6 +3,7 @@ USERID=$(id -u)
 if [$USERID -ne 0]
 then 
     echo "ERROR: YOU DO NOT HAVE THE PERMISSION TO INSTALL MYSQL. YOU NEED SUDO ACCESS"
+    exit 1
 fi
 dnf list installed mysql
 if [$? -ne 0]
