d87268345d78968cc13f811580fc2bfe646fca70
narendhar <narendhar.v@gmail.com>
2025-01-16 18:34:01 +0100

    Functions

---
+++ 11-functions.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+USERID=$(id -u)
+VALIDATE(){
+    if [$1 -ne 0]
+    then 
+        echo "$2...Failure"
+        exit 1
+    else 
+        echo "$2...success"
+    fi
+}
+if [$USERID -ne 0]
+then
+    echo "ERROR: YOU NEED TO LOGIN WITH SUDO"
+    exit 1
+fi
+dnf list installed mysql
+    if [$? -ne 0]
+    then 
+        dnf install mysql -y
+        VALIDATE $? "Installing MYSQL"
+    fi
+else
+    echo "Mysql is installed already"
+fi
+dnf list installed git 
+    if [$? -ne 0]
+    then 
+        dnf install git -y
+        VALIDATE $? "Installing Git"
+    fi
+else
+    echo "Git is installed already"
