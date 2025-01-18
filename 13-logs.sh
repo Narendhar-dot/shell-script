4d2e986746bb0f66bc5600fc49718b5662e5e84f
narendhar <narendhar.v@gmail.com>
2025-01-18 08:47:17 +0100

    a

--- 13-logs.sh
+++ 13-logs.sh
@@ -3,11 +3,11 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
+LOGS_FOLDER="/var/log/shell-script-logs"
 LOG_FILE=$(echo $0 | cut -d '.' -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
-LOGS_FOLDER="/var/log/shell-script-logs"
+
 USERID=$(id -u)
 
 VALIDATE(){
